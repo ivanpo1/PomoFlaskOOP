bb40ce8ea69336a1721b9c37c5f3b69b384771ea
ivanpo1 <ivan.poppino@gmail.com>
2024-09-19 01:30:49 -0300

    refactor timer.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,137 +1,119 @@
-import ProjectManager from './managers/ProjectManager.js';
-import TaskManager from './managers/TaskManager.js';
-import StateManager from './managers/StateManager.js';
-import UIManager from './managers/UIManager.js';
-import { Timer } from "./timer.js"
-import initializeEventListeners from './managers/EventListenerManager.js';
+import ProjectManager from "./managers/ProjectManager.js";
+import TaskManager from "./managers/TaskManager.js";
+import StateManager from "./managers/StateManager.js";
+import UIManager from "./managers/UIManager.js";
+import { Timer } from "./timer.js";
+import initializeEventListeners from "./managers/EventListenerManager.js";
 
-// Initialize StateManager
 const stateManager = new StateManager();
 const uiManager = new UIManager(stateManager);
 
-// Pass StateManager to other managers
 const projectManager = new ProjectManager(stateManager, uiManager);
 const taskManager = new TaskManager(stateManager, uiManager, projectManager);
-const timer = new Timer(stateManager)
+const timer = new Timer(stateManager);
 
 export { projectManager, taskManager, uiManager, timer };
 
-// initializeEventListeners();
-
 document.addEventListener("DOMContentLoaded", () => {
+  const timerSettings = document.querySelector(".settings-timer");
 
-    const timerSettings = document.querySelector(".settings-timer");
+  timerSettings.addEventListener("input", (event) => {
+    const { name, value } = event.target;
 
-    timerSettings.addEventListener("input", (event) => {
-
-      const { name, value } = event.target;
-
-      if (name && value) {
-        const numericValue = parseInt(value, 10);
-
-
-        timer.setTimerMode(name, numericValue);
-
-        console.log(`Setting ${name} to ${numericValue}`);
-      }
-
-    });
-
-    const tasksContainer = document.querySelector(".task-container");
-  
-    // Event delegation: handle clicks on task buttons
-    tasksContainer.addEventListener("click", (event) => {
-      if (event.target.classList.contains("btn-tasks")) {
-        console.log("target");
-        taskManager.handleBtnTaskClick(event.target);
-      }
-  
-      if (event.target.classList.contains("check-task")) {
-        taskManager.checkTask(event.target);
-      } 
-      
-      if (event.target.classList.contains("uncheck-task")) {
-        taskManager.uncheckTask(event.target);
-      }
-      
-      if (event.target.classList.contains("show-tasks")) {
-        uiManager.showCompletedTask(event.target);
-      }
-
-      // if (event.target.classList.contains("btn-delete")) {
-      //   taskManager.deleteTask(event.target);
-      // }
-
-      if (event.target.classList.contains("btn-delete-task")) {
-        uiManager.taskToDelete = event.target;
-        uiManager.showDeleteModal("Task");
-        // taskManager.deleteTask(event.target);
-      }
-      
-      
-    });
-    
-    const projectsContainer = document.querySelector('.projects-container');
-    
-    // Event delegation for project buttons
-    projectsContainer.addEventListener('click', (event) => {
-      // Check if the clicked element is a project button
-      if (event.target.classList.contains('btn-projects')) {
-        projectManager.handleBtnProjectClick(event.target);
-      }
-      
-      if (event.target.classList.contains("btn-delete-project")) {
-        // projectManager.deleteProject(event.target);
-        uiManager.projectToDelete = event.target;
-        uiManager.showDeleteModal("Project");
-      }
-    });
-
-    const confirmDeleteBtn = document.getElementById("confirmDelete");
-    confirmDeleteBtn.addEventListener("click", () => {
-      if (uiManager.projectToDelete) {
-        console.log("Delete confirmed", uiManager.projectToDelete);
-        projectManager.deleteProject(uiManager.projectToDelete);
-        uiManager.hideDeleteModal();
-        uiManager.projectToDelete = null;
-      }
-
-      if (uiManager.taskToDelete) {
-        console.log("Delete confirmed", uiManager.taskToDelete);
-        taskManager.deleteTask(uiManager.taskToDelete);
-        uiManager.hideDeleteModal();
-        uiManager.taskToDelete = null;
-      }
-    });
-
-    const cancelDeleteBtn = document.getElementById('cancelDelete');
-    cancelDeleteBtn.addEventListener('click', () => {
-      uiManager.hideDeleteModal();
-    });
-  });
-
-  const addTaskForm = document.querySelector('#addTaskForm')
-  addTaskForm.addEventListener("submit", async function (event) {
-    // stop form submission
-    event.preventDefault();
-  
-    // validate the form
-    let taskName = addTaskForm.elements["name_task"].value;
-    const task = await taskManager.addTask(taskName)
-    addTaskForm.reset();
-    if (task) {
-      // Example of adding the task to the UI
-      const taskItem = uiManager.createTaskItem(task, false); // Assuming new task is not completed
-      taskItem.classList.add('fade-in');
-      document.querySelector(".ul-task-list-incomplete").prepend(taskItem); // Add the task to the top of the list
-
-      setTimeout(() => {
-        taskItem.classList.add('visible');
-      }, 10);
+    if (name && value) {
+      const numericValue = parseInt(value, 10);
+      timer.setTimerMode(name, numericValue);
+      console.log(`Setting ${name} to ${numericValue}`);
     }
   });
 
-  // console.log(projectManager.fetchProjectData(sessionStorage.getItem("selectedProjectId")))
+  const tasksContainer = document.querySelector(".task-container");
+
+  tasksContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("btn-tasks")) {
+      console.log("target");
+      taskManager.handleBtnTaskClick(event.target);
+    }
+
+    if (event.target.classList.contains("check-task")) {
+      taskManager.checkTask(event.target);
+    }
+
+    if (event.target.classList.contains("uncheck-task")) {
+      taskManager.uncheckTask(event.target);
+    }
+
+    if (event.target.classList.contains("show-tasks")) {
+      uiManager.showCompletedTask(event.target);
+    }
+
+    // if (event.target.classList.contains("btn-delete")) {
+    //   taskManager.deleteTask(event.target);
+    // }
+
+    if (event.target.classList.contains("btn-delete-task")) {
+      uiManager.taskToDelete = event.target;
+      uiManager.showDeleteModal("Task");
+      // taskManager.deleteTask(event.target);
+    }
+  });
+
+  const projectsContainer = document.querySelector(".projects-container");
+
+  projectsContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("btn-projects")) {
+      projectManager.handleBtnProjectClick(event.target);
+    }
+
+    if (event.target.classList.contains("btn-delete-project")) {
+      // projectManager.deleteProject(event.target);
+      uiManager.projectToDelete = event.target;
+      uiManager.showDeleteModal("Project");
+    }
+  });
+
+  const confirmDeleteBtn = document.getElementById("confirmDelete");
+  confirmDeleteBtn.addEventListener("click", () => {
+    if (uiManager.projectToDelete) {
+      console.log("Delete confirmed", uiManager.projectToDelete);
+      projectManager.deleteProject(uiManager.projectToDelete);
+      uiManager.hideDeleteModal();
+      uiManager.projectToDelete = null;
+    }
+
+    if (uiManager.taskToDelete) {
+      console.log("Delete confirmed", uiManager.taskToDelete);
+      taskManager.deleteTask(uiManager.taskToDelete);
+      uiManager.hideDeleteModal();
+      uiManager.taskToDelete = null;
+    }
+  });
+
+  const cancelDeleteBtn = document.getElementById("cancelDelete");
+  cancelDeleteBtn.addEventListener("click", () => {
+    uiManager.hideDeleteModal();
+  });
+});
+
+const addTaskForm = document.querySelector("#addTaskForm");
+addTaskForm.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  let taskName = addTaskForm.elements["name_task"].value;
+  const task = await taskManager.addTask(taskName);
+  addTaskForm.reset();
+  if (task) {
+    const taskItem = uiManager.createTaskItem(task, false);
+    taskItem.classList.add("fade-in");
+    document.querySelector(".ul-task-list-incomplete").prepend(taskItem);
+
+    setTimeout(() => {
+      taskItem.classList.add("visible");
+    }, 10);
+  }
+});
+
+// console.log(projectManager.fetchProjectData(sessionStorage.getItem("selectedProjectId")))
 
 //   async function initPage() {
 //     const selectedProjectId = sessionStorage.getItem("selectedProjectId");
--- static/js/timer.js
+++ static/js/timer.js
@@ -1,226 +1,121 @@
-import { state } from "./managers/StateManager.js";
-
 export class Timer {
   constructor(stateManager) {
     this.stateManager = stateManager;
-    this.timer = {
-      pomodoro: 25,
+    this.timeMultiplier = 10; // Testing
+
+    this.timerSettings = {
+      pomodoro: 25, 
       shortRest: 5,
       longRest: 30,
-      session: 0,
-      untilLongRest: 4,
-      mode: null,
-    } 
+      untilLongRest: 4, 
+    };
 
-    this.currentTime = this.timer['pomodoro'] * 60 * 1000;
-    this.remainingTime = 0;
-    this.startTime = 0;
-    this.elapsedTime = 0;
-    // this.isTimerRunning = state.isTimerRunning;
+    this.sessionCount = 0; 
+    this.mode = "pomodoro";
+    this.currentTime = this.timerSettings[this.mode] * 60 * 1000; 
     this.isPaused = false;
     this.countdownInterval = null;
-    this.timerElement = document.getElementById("timer");
-    this.timerBox = document.getElementById("timer-box");
-    this.sessions = 0;
 
     this.init();
   }
 
   init() {
-    this.updateCountdown(this.timer['pomodoro'] * 60 * 1000);
+    this.updateCountdown(this.currentTime);
 
 
-    const startButton = document.querySelector(".btn-success");
-    const pauseButton = document.querySelector(".btn-warning");
-    const stopButton = document.querySelector(".btn-stop");
-
-    startButton.addEventListener("click", this.startCountdown.bind(this));
-    pauseButton.addEventListener("click", this.pauseCountdown.bind(this));
-    stopButton.addEventListener("click", this.stopCountdown.bind(this));
+    document.querySelector(".btn-success").addEventListener("click", this.startCountdown.bind(this));
+    document.querySelector(".btn-warning").addEventListener("click", this.pauseCountdown.bind(this));
+    document.querySelector(".btn-stop").addEventListener("click", this.stopCountdown.bind(this));
   }
 
-  switchTimer(mode) {
-    switch (mode) {
-      case 'pomodoro':
-        // Switch to rest period based on session count
-        if (this.timer.session >= this.timer.untilLongRest) {
-          this.currentTime = this.timer.longRest * 60 * 1000; // Long Rest
-          this.timer.mode = 'longRest';
-          document.querySelector('.title-timer').innerText = 'Long Rest';
-          this.timer.session = 0; // Reset session count after long rest
-        } else {
-          this.currentTime = this.timer.shortRest * 60 * 1000; // Short Rest
-          this.timer.mode = 'shortRest';
-          document.querySelector('.title-timer').innerText = 'Short Rest';
-        }
-        break;
 
-      case null:
-      case 'shortRest':
-      case 'longRest':
-        // After rest, switch back to pomodoro
-        this.currentTime = this.timer.pomodoro * 60 * 1000; // Pomodoro
-        this.timer.mode = 'pomodoro';
-        break;
+  switchMode() {
+    clearInterval(this.countdownInterval);
 
-      default:
-        console.error(`Unknown timer mode: ${mode}`);
+    if (this.mode === "pomodoro") {
+      this.sessionCount++;
     }
-  }
 
-  setTimerMode(mode, value) {
-    if (this.timer.hasOwnProperty(mode)) {
-      this.timer[mode] = value;
 
-    if (mode === 'pomodoro') {
-      let minutes = Math.floor(value)
-      .toString()
-      .padStart(2, "0");
-      let seconds = Math.floor(((value * 60 * 1000) / 1000) % 60)
-      .toString()
-      .padStart(2, "0");
-      this.timerElement.innerText = `${minutes}:${seconds}`;}
+    if (this.sessionCount >= this.timerSettings.untilLongRest) {
+      this.mode = "longRest";
+      this.sessionCount = 0; 
+    } else if (this.mode === "pomodoro") {
+      this.mode = "shortRest"; 
     } else {
-      console.error(`${mode} is not a valid timer mode`);
-    }}
+      this.mode = "pomodoro";
+    }
 
-  // updateSliderMinutes() {
-  //   this.timerBox.style.backgroundColor = "#22272e";
-  //   this.currentTime = Math.round(this.sliderMinutes.value * 60 * 1000);
-  //   this.updateCountdown(this.currentTime);
-  // }
+
+    this.currentTime = this.timerSettings[this.mode] * 60 * 1000;
+
+
+    document.querySelector(".title-timer").innerText = this.mode === "pomodoro" ? "Pomodoro" : this.mode === "shortRest" ? "Short Break" : "Long Break";
+    console.log(`Switched to ${this.mode} - Session: ${this.sessionCount}`);
+
+
+    this.stateManager.state.isTimerRunning = false;
+    this.startCountdown(); 
+  }
 
   startCountdown() {
-    // if (this.stateManager.state.isTimerRunning) return;
 
-    // clearInterval(this.countdownInterval);
+    if (this.stateManager.state.isTimerRunning) return;
 
-    // this.isTimerRunning = true;
     this.stateManager.state.isTimerRunning = true;
     this.isPaused = false;
-    this.timerBox.style.backgroundColor = "#687644";
-    this.startTime = this.currentTime;
-    this.taskId = this.stateManager.state.selectedTaskId;
-    this.projectId = this.stateManager.state.selectedProjectId;
-    this.switchTimer(this.timer.mode);
-
-    // this.logCurrentState("Start Button");
-
-    // const timeForCountdown =
-    //   this.remainingTime > 0 ? this.remainingTime : this.currentTime;
-    // console.log(`timeForCountdown: ${timeForCountdown}`)
+    
     this.countdown(this.currentTime);
   }
 
-  pauseCountdown() {
-    if (this.stateManager.state.isTimerRunning) {
-      this.timerBox.style.backgroundColor = "#9f7339";
-      this.isPaused = true;
-      this.isTimerRunning = false;
-      this.stateManager.state.isTimerRunning = false;
-    }
-
-    this.logCurrentState("Pause Button");
-  }
-
-  resetTimer() {
-    this.stateManager.state.isTimerRunning = false;
-    this.remainingTime = 0;
-    clearInterval(this.countdownInterval);
-    // this.updateCountdown(Math.round(sliderMinutes.value * 60 * 1000));
-  }
-
-  stopCountdown() {
-    clearInterval(this.countdownInterval);
-    this.updateCountdown(Math.round(this.currentTime));
-
-    this.logCurrentState("Stop Button");
-
-    this.stateManager.state.isTimerRunning = false;
-    this.elapsedTime = this.startTime - this.remainingTime;
-    this.remainingTime = 0;
-    this.timerBox.style.backgroundColor = "#531625";
-    this.taskId, this.projectId = this.stateManager.state.selectedTaskId;
-
-    this.sendDataFlask(this.elapsedTime);
-  }
-
   countdown(pTime) {
-    const endTime = Date.now() + pTime;
-  
+    const endTime = Date.now() + pTime / this.timeMultiplier;
+
     this.countdownInterval = setInterval(() => {
       if (!this.isPaused) {
         const currentTime = Date.now();
         const remainingTime = endTime - currentTime;
-  
+        
+
         if (remainingTime <= 0) {
           this.updateCountdown(0);
-          clearInterval(this.countdownInterval);
+          this.switchMode(); 
         } else {
-          this.updateCountdown(remainingTime);
+          this.updateCountdown(remainingTime * this.timeMultiplier); 
         }
-  
-        this.remainingTime = remainingTime;
       }
     }, 1000);
   }
 
-
-
   updateCountdown(pTime) {
-    if (pTime <= 0) {
-      // When the time runs out, switch the mode based on the current one
-      if (this.timer.mode === 'pomodoro') {
-        this.timer.session++; // Increase session count after each pomodoro
-        this.switchTimer('pomodoro');
-      } else if (this.timer.mode === 'shortRest') {
-        this.switchTimer('shortRest');
-      } else if (this.timer.mode === 'longRest') {
-        this.switchTimer('longRest');
+    const minutes = Math.floor(pTime / 60 / 1000).toString().padStart(2, "0");
+    const seconds = Math.floor((pTime / 1000) % 60).toString().padStart(2, "0");
+    document.getElementById("timer").innerText = `${minutes}:${seconds}`;
+  }
+
+  pauseCountdown() {
+    this.isPaused = true;
+    this.stateManager.state.isTimerRunning = false;
+  }
+
+  stopCountdown() {
+    clearInterval(this.countdownInterval);
+    this.isPaused = false;
+    this.stateManager.state.isTimerRunning = false;
+  }
+
+  setTimerMode(mode, value) {
+    if (this.timerSettings.hasOwnProperty(mode)) {
+      this.timerSettings[mode] = parseInt(value);
+  
+      
+      if (this.mode === mode) {
+        this.currentTime = parseInt(value) * 60 * 1000;
+        this.updateCountdown(this.currentTime); 
       }
-  
-      // Reset the timer display and prepare for the next countdown
-      this.timerElement.innerText = "00:00";
-      this.timerBox.style.backgroundColor = "#531625";
-      // this.sendDataFlask(this.startTime); // Send data to Flask on completion
-      document.querySelector('.title-timer').innerText = 'Rest';
-      this.timerBox.style.backgroundColor = "#2b405e";
-  
-      // Clear the current interval
-      clearInterval(this.countdownInterval);
-  
-      // Update the countdown with the new current time (based on the mode)
-      this.updateCountdown(this.currentTime);
-  
-      // Automatically start the countdown for the next session
-      this.startCountdown(); // Automatically restart the countdown
     } else {
-      // Update the display with the remaining time
-      let minutes = Math.floor(pTime / 60 / 1000)
-        .toString()
-        .padStart(2, "0");
-      let seconds = Math.floor((pTime / 1000) % 60)
-        .toString()
-        .padStart(2, "0");
-      this.timerElement.innerText = `${minutes}:${seconds}`;
+      console.error(`${mode} is not a valid timer mode`);
     }
   }
-
-  sendDataFlask(startTime) {
-    const timeToSend = startTime || this.elapsedTime;
-    const projectSelect = document.getElementById("project-selector");
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "/save_time", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(
-      JSON.stringify({
-        time: timeToSend,
-        date: new Date(),
-        project_id: this.projectId,
-        task_id: this.taskId,
-      })
-    );
-  }
-  
 }
 
